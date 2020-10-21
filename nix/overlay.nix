adb15a7ebd9483878d28234727863ad4630b09d2
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-10-21 15:05:45 +0200

    Removed some now-obsolete nix code

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -2,28 +2,6 @@ final: previous:
 with final.lib;
 with final.haskell.lib;
 let
-  openAPICodeGeneratorRepo = builtins.fetchGit {
-    url = "https://github.com/Haskell-OpenAPI-Code-Generator/Haskell-OpenAPI-Client-Code-Generator";
-    rev = "c3133bde91430990ff9c03c19a015713ae291e2f";
-    ref = "master";
-  };
-  openAPICodeGenerator = dontHaddock (disableLibraryProfiling (final.haskellPackages.callCabal2nix "openapi3-code-generator" (final.gitignoreSource ../.) { }));
-  generatedStripeCode = final.stdenv.mkDerivation {
-    name = "generated-stripe-code";
-    buildInputs = [
-      openAPICodeGenerator
-      final.tree
-    ];
-    buildCommand = ''
-      # To make sure that we don't get issues with encodings
-      export LANG=C.utf8
-      export LC_ALL=C.utf8
-
-      ${../scripts/generate-stripe.sh} ${openAPICodeGeneratorRepo} $out
-    '';
-  };
-  generatedStripePackage = dontHaddock (disableLibraryProfiling (final.haskellPackages.callCabal2nix "stripe-api" generatedStripeCode { }));
-
 in
 {
   generateOpenAPIClient = import ./generate-client.nix { pkgs = final; };
@@ -43,7 +21,7 @@ in
             (
               self: super:
                 {
-                  openapi3-code-generator = openAPICodeGenerator;
+                  openapi3-code-generator = final.haskellPackages.callCabal2nix "openapi3-code-generator" (final.gitignoreSource ../.) { };
                 }
             );
       }
