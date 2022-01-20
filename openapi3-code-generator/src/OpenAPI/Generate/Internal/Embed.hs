fda5e416c3a2308c6db4ad080e1977e2b43719aa
Joel Fisch <6702422+joelfisch@users.noreply.github.com>
2022-01-20 08:26:14 +0100

    Use Data.Version to read the version number instead of parsing it from package.yaml

--- openapi3-code-generator/src/OpenAPI/Generate/Internal/Embed.hs
+++ openapi3-code-generator/src/OpenAPI/Generate/Internal/Embed.hs
@@ -1,20 +1,7 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module OpenAPI.Generate.Internal.Embed where
 
-import Data.Maybe
-import qualified Data.Text as T
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 
 embedFile :: FilePath -> Q Exp
 embedFile fp = LitE . StringL <$> (qAddDependentFile fp >> runIO (readFile fp))
-
-embedPackageVersionNumber :: FilePath -> Q Exp
-embedPackageVersionNumber fp = do
-  fileContents <- qAddDependentFile fp >> runIO (readFile fp)
-  let versionNumberLines = mapMaybe (fmap T.strip . T.stripPrefix "version:") $ T.lines (T.pack fileContents)
-      version = case versionNumberLines of
-        v : _ -> T.unpack v
-        [] -> fail $ "Could not extract version number from file: " <> fp
-  litE $ StringL version
