88e3fc332d27c08198beadb3484c31f41ddb90f5
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-10-26 23:06:46 +0200

    expose generation function as a passthru

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -3,7 +3,11 @@ with final.lib;
 with final.haskell.lib;
 {
   generateOpenAPIClient = final.callPackage ./generate-client.nix { };
-  openapi3-code-generator = justStaticExecutables final.haskellPackages.openapi3-code-generator;
+  openapi3-code-generator = (justStaticExecutables final.haskellPackages.openapi3-code-generator).overrideAttrs (old: {
+    passthru = (old.passthru or { }) // {
+      generateOpenAPIClient = final.generateOpenAPIClient;
+    };
+  });
   haskellPackages = prev.haskellPackages.override (old: {
     overrides = final.lib.composeExtensions (old.overrides or (_: _: { })) (
       self: super: {
