280ee297f7510e61d58da5e35b83b95a8be97eba
Joel Bach <6702422+joel-bach@users.noreply.github.com>
2024-04-27 11:00:20 +0200

    Support macOS for development

--- update_golden_and_example.sh
+++ update_golden_and_example.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-nix build .\#checks.x86_64-linux.testGoldenGenerate
+system=$(nix eval --impure --raw --expr 'builtins.currentSystem')
+
+nix build .\#checks.${system}.testGoldenGenerate
 rm -rf testing/golden-output
 mkdir -p testing/golden-output
 cp -R result/* testing/golden-output
 chmod -R 764 testing/golden-output
 
 
-nix build .\#checks.x86_64-linux.exampleGenerate
+nix build .\#checks.${system}.exampleGenerate
 rm -rf example/generatedCode
 mkdir -p example/generatedCode
 cp -R result/* example/generatedCode
